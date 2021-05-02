a843fc0c9e59817333a1512752bc538664b266d0
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2021-05-02 17:39:30 +0530

    Add files via upload

---
+++ Day 2/set.py
@@ -0,0 +1,18 @@
+# creating a set
+fruits={"apple","jack","berry",2,3,2,4 }
+print(fruits)
+
+# adding or removing an element
+# fruits.add(12)
+fruits.update([12,13])
+
+# removing
+fruits.remove(13)
+# fruits.clear()
+print(fruits)
+
+vegi={"ciona","tunip"}
+
+# union
+union =fruits | vegi
+print(union)
